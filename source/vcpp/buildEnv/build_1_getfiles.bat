746129631384077c8867622294c4d6af5479bfe1
William Narmontas <william@scalawilliam.com>
2017-01-26 20:23:21 +0800

    Remove redundancy

--- source/vcpp/buildEnv/build_1_getfiles.bat
+++ source/vcpp/buildEnv/build_1_getfiles.bat
@@ -1,12 +1,9 @@
 set tmp_ac=%TEMP%\af
 rd /s /q %tmp_ac%
 mkdir %tmp_ac%
-echo D | xcopy ..\..\.. %tmp_ac% /s /e /q
-rd /s /q %tmp_ac%\source\vcpp\buildEnv\ac
-rd /s /q %tmp_ac%\source\vcpp\Debug
-rd /s /q %tmp_ac%\source\vcpp\Release
-rd /s /q %tmp_ac%\.git
 rd /s /q ac
+echo D | xcopy ..\..\.. %tmp_ac% /s /e /q
+rd /s /q %tmp_ac%\.git
 echo D | xcopy %tmp_ac% ac /s /e /y /q
 cd ac
 cd source/vcpp/buildEnv/ac/
