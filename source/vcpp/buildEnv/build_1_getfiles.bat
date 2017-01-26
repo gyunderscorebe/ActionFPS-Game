5259d6f1f02c24a73cb197f09bb6a0ba2a6c41ef
William Narmontas <william@scalawilliam.com>
2017-01-26 20:05:20 +0800

    Remove UNZIP and Git dependency from build step 1

--- source/vcpp/buildEnv/build_1_getfiles.bat
+++ source/vcpp/buildEnv/build_1_getfiles.bat
@@ -1,9 +1,11 @@
+set tmp_ac=%TEMP%\af
+rd /s /q %tmp_ac%
+mkdir %tmp_ac%
+echo D | xcopy ..\..\.. %tmp_ac% /s /e /q
 rd /s /q ac
-mkdir ac
-cd ../../..
-git archive -o source/vcpp/buildEnv/ac.zip HEAD
+echo D | xcopy %tmp_ac% ac /s /e /y /q
+cd ac
 cd source/vcpp/buildEnv/ac/
-unzip.exe ../ac.zip
 cd source/vcpp
 \Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe cube.sln /p:Configuration=Release
 cd ../launcher
