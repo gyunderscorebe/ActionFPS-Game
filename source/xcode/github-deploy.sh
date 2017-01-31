08232376fd6b7e9dd2b5b7b8fd10d6b47c5a98a5
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-02-01 06:44:23 +0800

    Add jq to circle build

--- source/xcode/github-deploy.sh
+++ source/xcode/github-deploy.sh
@@ -4,7 +4,7 @@ if [ "$CIRCLE_TAG" ]; then
   TAG_ID=$CIRCLE_TAG
   SOURCE_NAME=./build/Release/actionfps.dmg
   TARGET_NAME=actionfps.dmg
-  RELEASE_ID=$(curl https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/64_bit_windows | jq '.id')
+  RELEASE_ID=$(curl https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/$TAG_ID | jq '.id')
   curl -H "Authorization: token $GITHUB_RELEASE_TOKEN" \
        -H "Accept: application/vnd.github.manifold-preview" \
        -H "Content-Type: application/octet-stream" \
