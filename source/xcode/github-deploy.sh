d539a787ebad979aa2a04c67f1d67f8255cae46e
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-02-01 06:15:44 +0800

    Upload .dmg artifact to github. #84

---
+++ source/xcode/github-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$CIRCLE_TAG" ]; then
+  TAG_ID=$CIRCLE_TAG
+  SOURCE_NAME=./build/Release/actionfps.dmg
+  TARGET_NAME=actionfps.dmg
+  RELEASE_ID=$(curl https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/64_bit_windows | jq '.id')
+  curl -H "Authorization: token $GITHUB_RELEASE_TOKEN" \
+       -H "Accept: application/vnd.github.manifold-preview" \
+       -H "Content-Type: application/octet-stream" \
+       --data-binary @$SOURCE_NAME \
+       "https://uploads.github.com/repos/ActionFPS/ActionFPS-Game/releases/$RELEASE_ID/assets?name=$TARGET_NAME"
+fi
