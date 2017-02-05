d8823f4cc44e532a655d761140296e7b3a0f096e
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-02-05 22:23:29 +0800

    Fail build if deploy fails for osx

--- source/xcode/github-deploy.sh
+++ source/xcode/github-deploy.sh
@@ -4,7 +4,10 @@ if [ "$CIRCLE_TAG" ]; then
   TAG_ID=$CIRCLE_TAG
   SOURCE_NAME=./build/Release/actionfps.dmg
   TARGET_NAME=actionfps.dmg
-  RELEASE_ID=$(curl https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/$TAG_ID | jq '.id')
+  curl --fail https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/$TAG_ID || sleep 30
+  curl --fail https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/$TAG_ID || sleep 60
+  curl --fail https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/$TAG_ID || sleep 90
+  RELEASE_ID=$(curl --fail https://api.github.com/repos/ActionFPS/ActionFPS-Game/releases/tags/$TAG_ID | jq '.id')
   curl -H "Authorization: token $GITHUB_RELEASE_TOKEN" \
        -H "Accept: application/vnd.github.manifold-preview" \
        -H "Content-Type: application/octet-stream" \
